9fb78ab63de11f129be56280d183a27b757336a8
PremPredics <sidequestvas@gmail.com>
2026-05-27 01:31:31 +0100

    Force Android status and navigation bar colours

--- android/app/src/main/java/com/prempredics/app/MainActivity.java
+++ android/app/src/main/java/com/prempredics/app/MainActivity.java
@@ -1,5 +1,19 @@
 package com.prempredics.app;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#2E1065"));
+        window.setNavigationBarColor(Color.parseColor("#2E1065"));
+        window.getDecorView().setSystemUiVisibility(0);
+    }
+}
