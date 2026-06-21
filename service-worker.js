50f84313327de88ccbde4ce5371fd0140df2ed91
PremPredics <sidequestvas@gmail.com>
2026-06-22 00:12:29 +0100

    Add Super Draw gold sand transition

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'prem-predics-pwa-v18';
+const CACHE_VERSION = 'prem-predics-pwa-v19';
 const APP_CACHE = `${CACHE_VERSION}-app`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
